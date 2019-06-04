74794b701cba3540395e3af69932acb1a3f0f6ae
ewelina.slepko <ewelina.slepko1@gmail.com>
2019-06-04 21:32:13 +0200

    connected components with appropriate path

--- src/layouts/Page.jsx
+++ src/layouts/Page.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
 import HomePage from '../pages/HomePage';
+import CookiesAndCupcakes from '../pages/CookiesAndCupcakes';
+import ArtCorner from '../pages/ArtCorner';
+import Contact from '../pages/Contact';
+import Coffee from '../pages/Coffee';
 
 const Page = () => {
     return (
         <>
             <Route path="/" exact component={HomePage} />
-            <Route path="/" exact component={HomePage} />
-            <Route path="/" exact component={HomePage} />
-            <Route path="/" exact component={HomePage} />
-            <Route path="/" exact component={HomePage} />
+            <Route path="/coffee" exact component={Coffee} />
+            <Route path="/cookies&cupcakes" exact component={CookiesAndCupcakes} />
+            <Route path="/artcorner" exact component={ArtCorner} />
+            <Route path="/contact" exact component={Contact} />
         </>
     );
 }
