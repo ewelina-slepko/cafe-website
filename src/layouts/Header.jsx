9faca0f8ddef3df2895e257383054f08156e42e8
ewelina.slepko <ewelina.slepko1@gmail.com>
2019-06-04 20:24:15 +0200

    Changed images

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -6,12 +6,11 @@ import image1 from '../images/image1.jpg'
 import image2 from '../images/image2.jpg'
 import image3 from '../images/image3.jpg'
 import image4 from '../images/image4.jpg'
-import image5 from '../images/image5.jpg'
-import image6 from '../images/image6.jpg'
+
 
 import uuid from "uuid/v1";
 
-const images = [image1, image2, image3, image4, image5, image6].map(image => ({ id: uuid(), src: image }))
+const images = [image1, image2, image3, image4].map(image => ({ id: uuid(), src: image }))
 
 class Header extends Component {
     state = {
@@ -29,7 +28,7 @@ class Header extends Component {
                     selected: images[newIndex]
                 };
             });
-        }, 3000)
+        }, 4000)
     };
 
     render = () => (
