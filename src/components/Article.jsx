b6caae4cc6c6cde9bbaee93b0323dec052f872fb
ewelina.slepko <ewelina.slepko1@gmail.com>
2019-06-05 22:16:59 +0200

    Modified ContactPage Component

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -3,15 +3,30 @@ import React from 'react'
 const Article = ({ img, title, author, text }) => {
 
     const styles = {
-        marginTop: 50
+        marginTop: 50,
     }
 
     return (
         <article style={styles}>
-            <img src={img} alt={img} />
-            <h1>{title}</h1>
-            <span>{author}</span>
-            <p>{text}</p>
+            <img style={{
+                width: "50vh",
+                height: "40vh"
+            }} src={img} alt={img} />
+
+            <h1 style={{
+                textTransform: "uppercase",
+                marginBottom: 3
+            }}>{title}</h1>
+
+            <span style={{
+                display: "block",
+                marginBottom: 10,
+                fontSize: 14
+            }}>{author}</span>
+
+            <p style={{
+                fontSize: 16,
+            }}>{text}</p>
         </article>
     );
 }
--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import '../styles/contactPage.css';
 
-const Contact = () => {
-    return (
-        <div>Contact</div>
-    );
+class Contact extends React.Component {
+    state = {
+        value: ''
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            value: e.target.value
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
+    render() {
+        return (
+            <div className="contact" >
+                <form>
+                    <h3>Write us a message</h3>
+                    <textarea onChange={this.handleChange} value={this.state.value} placeholder="message"></textarea>
+                    <button onClick={this.handleSubmit}>Send</button>
+                </form>
+            </div>
+
+        );
+    }
 }
 
 export default Contact;
