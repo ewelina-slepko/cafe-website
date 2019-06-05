64eaf7a3a84893de05bdb9ad3a32c7235aafc0c3
ewelina.slepko <ewelina.slepko1@gmail.com>
2019-06-05 23:00:46 +0200

    styled button, added Prompt message before quit

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Prompt } from 'react-router-dom'
 import '../styles/contactPage.css';
 
 class Contact extends React.Component {
@@ -21,9 +22,12 @@ class Contact extends React.Component {
             <div className="contact" >
                 <form>
                     <h3>Write us a message</h3>
-                    <textarea onChange={this.handleChange} value={this.state.value} placeholder="message"></textarea>
-                    <button onClick={this.handleSubmit}>Send</button>
+                    <textarea onChange={this.handleChange} value={this.state.value} placeholder="message..."></textarea>
+                    <button onClick={this.handleSubmit}><span>Send</span></button>
                 </form>
+                <Prompt
+                    when={this.state.value}
+                    message="If you change the path you'll lose your message. Are you sure you want to quit?" />
             </div>
 
         );
