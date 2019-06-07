71f9a5092adbc4debbbdbe01206920b366a663fd
ewelina.slepko <ewelina.slepko1@gmail.com>
2019-06-07 21:03:45 +0200

    Made a footer

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import '../styles/footer.css'
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <div>
+            <span>Phone number: +775068394</span><span>email: artcafe@gmail.com</span>
+            <p>Art Cafe, 4856 Lakeland Park Drive Street, Gainesville, Georgia (GA), 30501</p>
+            <p>copyright© artcafe.com</p>
+        </div>
     );
 }
 
