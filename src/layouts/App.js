7acbef7b23e36b77a8218a12b052b04735f105ad
ewelina.slepko <ewelina.slepko1@gmail.com>
2019-06-08 23:49:04 +0200

    footer changes

--- src/layouts/App.js
+++ src/layouts/App.js
@@ -25,7 +25,6 @@ function App() {
         </footer>
       </div>
     </Router>
-
   );
 }
 
--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -3,20 +3,36 @@ import '../styles/footer.css'
 
 const Footer = () => {
     return (
-        <div className="footer_content">
-            <div className="section">
-                Contact us!
-            <p>Phone number: +775068394</p>
-                <p>email: artcafe@gmail.com</p>
-                <p>Art's Corner Cafe, 4856 Lakeland Park Drive Street, Gainesville, Georgia (GA), 30501</p>
+        <>
+            <div className="footer_container">
+                <div className="footer_content">
+                    <div className="section">
+                        <h3>Contact us!</h3>
+                        <p>Phone number: +775068394</p>
+                        <p>email: artcafe@gmail.com</p>
+                        <p>Art's Corner Cafe, 4856 Lakeland Park Drive Street, Gainesville, Georgia (GA), 30501</p>
+                    </div>
+                    <div className="section">
+                        <h3>Contact us!</h3>
+                        <p>csdvdsvdsgsd sdgsdgsdgsd adfdsgsdgsdgs sgsfg sfgdffsfssfgsf</p>
+                    </div>
+                    <div className="section">
+                        <h3>Social Media</h3>
+                        <p>something</p>
+                    </div>
+                    <div className="links">
+                        <h3>Terms and conditions</h3>
+                        <h3>Privacy policy</h3>
+                        <h3>Wifi term of use</h3>
+                    </div>
+                </div>
+
+
             </div>
-            <div className="section">
-                <p>copyright© artcafe.com</p>
+            <div className="copyright">
+                copyright© artscorner.com
             </div>
-            <div className="section">
-                <p>something</p>
-            </div>
-        </div>
+        </>
     );
 }
 
