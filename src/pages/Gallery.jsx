06d0a0c2ba154b6284f24797f8142af2ee3c236b
ewelina.slepko <ewelina.slepko1@gmail.com>
2019-06-11 00:57:16 +0200

    Made a gallery using map method

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,9 +1,39 @@
 import React from 'react';
+import '../styles/gallery.css'
+
+import gallery1 from '../images/gallery/gallery1.jpg'
+import gallery2 from '../images/gallery/gallery2.jpg'
+import gallery3 from '../images/gallery/gallery3.jpg'
+import gallery4 from '../images/gallery/gallery4.jpg'
+import gallery5 from '../images/gallery/gallery5.jpg'
+import gallery6 from '../images/gallery/gallery6.jpg'
+import gallery7 from '../images/gallery/gallery7.jpg'
+import gallery8 from '../images/gallery/gallery8.jpg'
+import gallery9 from '../images/gallery/gallery9.jpg'
+import gallery10 from '../images/gallery/gallery10.jpg'
+import gallery11 from '../images/gallery/gallery11.jpg'
+import gallery12 from '../images/gallery/gallery12.jpg'
+import gallery13 from '../images/gallery/gallery13.jpg'
+import gallery14 from '../images/gallery/gallery14.jpg'
+import gallery15 from '../images/gallery/gallery15.jpg'
+
+
 
 const Gallery = () => {
+    const fullGallery = [gallery1, gallery2, gallery3, gallery4, gallery5, gallery6, gallery7,
+        gallery8, gallery9, gallery10, gallery11, gallery12, gallery13, gallery14, gallery15].map(img => {
+            return <img
+                src={img}
+                alt={img}
+                className="img-responsive" />
+        });
     return (
-        <div>Beautiful gallery!</div>
+        <>
+            <div>Beautiful gallery!</div>
+            <div>{fullGallery}</div>
+        </>
     );
 }
 
 export default Gallery;
+
