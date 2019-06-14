2e007ed71da53529a212ac6cd00311786a35be2c
ewelina.slepko <ewelina.slepko1@gmail.com>
2019-06-14 22:50:11 +0200

    Modified styles on Art Corner Page

--- src/pages/ArtCornerPage.jsx
+++ src/pages/ArtCornerPage.jsx
@@ -24,16 +24,34 @@ const ArtCorner = () => {
                         We promote and support young aspiring artist by giving them a platform to show there work.
                         Come to us and create fine arts, painting and drawing. illustrations, portraits, and much more.</p>
 
-                    <img src={art_corner2} alt={art_corner2} className="section-img" />
                     <p className="section-text"> Our Art Lessons explore a variety of drawing and painting techniques across a range of
                     subjects and styles. In each lesson we lead you through an illustrated step by step development of the artwork
-                    from its initial stages to the finished example.</p>
+                    from its initial stages to the finished example. Our Art Lessons explore a variety of drawing and painting techniques across a range of
+                    subjects and styles. In each lesson we lead you through an illustrated step by step development of the artwork
+                        from its initial stages to the finished example.</p>
+                    <img src={art_corner2} alt={art_corner2} className="section-img" />
 
-                    <img src={art_corner3} alt={art_corner3} className="section-img" />
-                    <p className="section-text"></p>
+
 
                     <img src={art_corner4} alt={art_corner4} className="section-img" />
-                    <p className="section-text"></p>
+                    <p className="section-text" > Our Art Lessons explore a variety of drawing and painting techniques across a range of
+                    subjects and styles. In each lesson we lead you through an illustrated step by step development of the artwork
+                    from its initial stages to the finished example. Our Art Lessons explore a variety of drawing and painting techniques across a range of
+                    subjects and styles. In each lesson we lead you through an illustrated step by step development of the artwork
+                        from its initial stages to the finished example.</p>
+
+
+                    <div style={{ width: '100%', padding: '50px 0 100px 0', backgroundColor: '#ededed' }} >
+                        <ul className="section-list">
+                            <h1 style={{ padding: '40px', background: 'none' }}>Course Outcomes</h1>
+                            <li>Research best practice, including basic, advanced, and color theory techniques, for watercolor painting in the classroom setting.</li>
+                            <li>Explore ideation strategies for developing concept-based lessons and artwork.</li>
+                            <li>Create resources and hands-on examples to use directly in the classroom.</li>
+                            <li>Develop or extend your watercolor curriculum.</li>
+                            <li>Push your personal practice to the next level by working in a visual journal with watercolor.</li>
+                            <li>Create original watercolor artwork while experimenting with new techniques.</li>
+                        </ul>
+                    </div>
 
                 </div >
             </ScrollIntoViewIfNeeded>
