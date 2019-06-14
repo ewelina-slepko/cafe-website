32ed613ae9d56d1382832030e6240eb5538df0e4
ewelina.slepko <ewelina.slepko1@gmail.com>
2019-06-14 21:07:18 +0200

    Modified flexbox on Art Corner Page

--- src/pages/ArtCornerPage.jsx
+++ src/pages/ArtCornerPage.jsx
@@ -16,19 +16,25 @@ const ArtCorner = () => {
             }}>
                 <div className="artcorner_container">
 
-                    <img src={art_corner1} alt={art_corner1} className="section-responsive" />
-                    <p className="section-responsive">The Art Corner is an artist's paradise.
+                    <img src={art_corner1} alt={art_corner1} className="section-img" />
+                    <p className="section-text">The Art Corner is an artist's paradise.
                         We carry a wide variety of professional and student grade art and craft supplies. The Art Corner's goal
-                is to inspire and nurture everyone's artist within through their motto "Be Creative, Think Young."</p>
+                        is to inspire and nurture everyone's artist within through their motto "Be Creative, Think Young.
+                        "We also specialize in teaching the visual arts to kids, teens, adults and seniors.
+                        We promote and support young aspiring artist by giving them a platform to show there work.
+                        Come to us and create fine arts, painting and drawing. illustrations, portraits, and much more.</p>
 
-                    <p className="section-responsive"> We also specialize in teaching the visual arts to kids, teens, adults and seniors.</p>
-                    <img src={art_corner2} alt={art_corner2} className="section-responsive" />
+                    <img src={art_corner2} alt={art_corner2} className="section-img" />
+                    <p className="section-text"> Our Art Lessons explore a variety of drawing and painting techniques across a range of
+                    subjects and styles. In each lesson we lead you through an illustrated step by step development of the artwork
+                    from its initial stages to the finished example.</p>
 
-                    <img src={art_corner3} alt={art_corner3} className="section-responsive" />
-                    <p className="section-responsive">We promote and support young aspiring artist by giving them a platform to show there work.</p>
+                    <img src={art_corner3} alt={art_corner3} className="section-img" />
+                    <p className="section-text"></p>
 
-                    <p className="section-responsive">Come to us and create fine arts, painting and drawing. illustrations, portraits, and much more.</p>
-                    <img src={art_corner4} alt={art_corner4} className="section-responsive" />
+                    <img src={art_corner4} alt={art_corner4} className="section-img" />
+                    <p className="section-text"></p>
+
                 </div >
             </ScrollIntoViewIfNeeded>
         </>
